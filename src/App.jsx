4b8413b975891d49e7bd360fa7b5c3bff15c7e4a
Shaheed mon <sheheedk07@gmail.com>
2024-12-29 08:26:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-import HeroPage from './componets/hero/HeroPage'
-import About from './componets/about/About'
-import Skills from './componets/skills/Skills'
-import Projects from './componets/projects/Projects'
-import Contact from './componets/contact/Contact'
-import Footer from './componets/footer/Footer'
+import HeroPage from "./componets/hero/HeroPage";
+import About from "./componets/about/About";
+import Skills from "./componets/skills/Skills";
+import Projects from "./componets/projects/Projects";
+import Contact from "./componets/contact/Contact";
+import Footer from "./componets/footer/Footer";
 
 function App() {
   return (
     <>
-
-
-<HeroPage/> 
-<About/>
-<Skills></Skills>
-<Projects/> 
-<Contact/>
-<Footer/>
-
+      <HeroPage />
+      <About />
+      <Skills></Skills>
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/componets/hero/HeroPage.jsx
+++ src/componets/hero/HeroPage.jsx
@@ -52,4 +52,3 @@ const HeroPage = () => {
 };
 
 export default HeroPage;
-
