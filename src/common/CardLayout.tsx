c32acf537952c3c5a210c42ab818ce90c01d237f
Shaheed mon <sheheedk07@gmail.com>
2025-05-08 17:26:31 +0530

    final

--- src/common/CardLayout.tsx
+++ src/common/CardLayout.tsx
@@ -5,11 +5,15 @@ interface CardLayoutProps {
   className?: string;
 }
 
-const CardLayout: React.FC<CardLayoutProps> = ({ children, className = "" }) => {
+const CardLayout: React.FC<CardLayoutProps> = ({
+  children,
+  className = "",
+}) => {
   return (
     <div
-    className={`bg-white/10 backdrop-blur-md rounded-xl p-6 shadow-lg w-[120px] h-[120px] flex flex-col justify-center items-center 
-      transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl hover:bg-transparent hover:border   ${className}`} >
+      className={`bg-white/10 backdrop-blur-md rounded-xl p-6 shadow-lg w-[120px] h-[120px] flex flex-col justify-center items-center  
+     transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl hover:bg-transparent hover:border   ${className}`}
+    >
       {children}
     </div>
   );
